38edffa8b96416d87b27a2fe8c91a04ffc0809f2
徐梓琅 <ksbc24v@gmail.com>
2019-11-14 09:46:11 +0800

    make this script adjust MG production

--- utils/voc/converter.py
+++ utils/voc/converter.py
@@ -16,6 +16,7 @@ import argparse
 import glog as log
 from lxml import etree
 from pascal_voc_writer import Writer
+import shutil
 
 
 def parse_args():
@@ -42,7 +43,7 @@ def parse_args():
     return parser.parse_args()
 
 
-def process_cvat_xml(xml_file, image_dir, output_dir):
+def process_cvat_xml(xml_dir, image_dir, output_dir):
     """
     Transforms a single XML in CVAT format to multiple PASCAL VOC format
     XMls.
@@ -53,100 +54,109 @@ def process_cvat_xml(xml_file, image_dir, output_dir):
     :return:
     """
     KNOWN_TAGS = {'box', 'image', 'attribute'}
-    os.makedirs(output_dir, exist_ok=True)
-    cvat_xml = etree.parse(xml_file)
+    output_xml_dir = os.path.join(output_dir, 'xml')
+    output_img_dir = os.path.join(output_dir, 'img')
+    os.makedirs(output_xml_dir, exist_ok=True)
+    os.makedirs(output_img_dir, exist_ok=True)
+    for xml in os.listdir(xml_dir):
+        xml_file = os.path.join(xml_dir, xml)
+        cvat_xml = etree.parse(xml_file)
 
-    basename = os.path.splitext( os.path.basename( xml_file ) )[0]
+        basename = os.path.splitext( os.path.basename( xml_file ) )[0]
 
-    tracks= cvat_xml.findall( './/track' )
+        tracks= cvat_xml.findall( './/track' )
 
-    if (tracks is not None) and (len(tracks) > 0):
-        frames = {}
+        if (tracks is not None) and (len(tracks) > 0):
+            frames = {}
 
-        for track in tracks:
-            trackid = int(track.get("id"))
-            label = track.get("label")
-            boxes = track.findall( './box' )
-            for box in boxes:
-                frameid  = int(box.get('frame'))
-                outside  = int(box.get('outside'))
-                #occluded = int(box.get('occluded'))  #currently unused
-                #keyframe = int(box.get('keyframe'))  #currently unused
-                xtl      = float(box.get('xtl'))
-                ytl      = float(box.get('ytl'))
-                xbr      = float(box.get('xbr'))
-                ybr      = float(box.get('ybr'))
-                
-                frame = frames.get( frameid, {} )
-                
-                if outside == 0:
-                    frame[ trackid ] = { 'xtl': xtl, 'ytl': ytl, 'xbr': xbr, 'ybr': ybr, 'label': label }
+            for track in tracks:
+                trackid = int(track.get("id"))
+                label = track.get("label")
+                boxes = track.findall( './box' )
+                for box in boxes:
+                    frameid  = int(box.get('frame'))
+                    outside  = int(box.get('outside'))
+                    #occluded = int(box.get('occluded'))  #currently unused
+                    #keyframe = int(box.get('keyframe'))  #currently unused
+                    xtl      = float(box.get('xtl'))
+                    ytl      = float(box.get('ytl'))
+                    xbr      = float(box.get('xbr'))
+                    ybr      = float(box.get('ybr'))
+                    
+                    frame = frames.get( frameid, {} )
+                    
+                    if outside == 0:
+                        frame[ trackid ] = { 'xtl': xtl, 'ytl': ytl, 'xbr': xbr, 'ybr': ybr, 'label': label }
 
-                frames[ frameid ] = frame
+                    frames[ frameid ] = frame
 
-        width = int(cvat_xml.find('.//original_size/width').text)
-        height  = int(cvat_xml.find('.//original_size/height').text)
+            width = int(cvat_xml.find('.//original_size/width').text)
+            height  = int(cvat_xml.find('.//original_size/height').text)
 
-        # Spit out a list of each object for each frame
-        for frameid in sorted(frames.keys()):
-            #print( frameid )
+            # Spit out a list of each object for each frame
+            for frameid in sorted(frames.keys()):
+                #print( frameid )
 
-            image_name = "%s_%08d.jpg" % (basename, frameid)
-            image_path = os.path.join(image_dir, image_name)
-            if not os.path.exists(image_path):
-                log.warn('{} image cannot be found. Is `{}` image directory correct?'.
-                    format(image_path, image_dir))
-            writer = Writer(image_path, width, height)
+                image_name = "%s_%08d.jpg" % (basename, frameid)
+                image_path = os.path.join(image_dir, image_name)
+                output_image_path = os.path.join(output_img_dir, os.path.basename(image_name))
+                shutil.copy(image_path, output_image_path)
+                if not os.path.exists(image_path):
+                    log.warn('{} image cannot be found. Is `{}` image directory correct?'.
+                        format(image_path, image_dir))
+                writer = Writer(image_path, width, height)
 
-            frame = frames[frameid]
+                frame = frames[frameid]
 
-            objids = sorted(frame.keys())
+                objids = sorted(frame.keys())
 
-            for objid in objids:
+                for objid in objids:
 
-                box = frame[objid]
+                    box = frame[objid]
 
-                label = box.get('label')
-                xmin = float(box.get('xtl'))
-                ymin = float(box.get('ytl'))
-                xmax = float(box.get('xbr'))
-                ymax = float(box.get('ybr'))
+                    label = box.get('label')
+                    xmin = int(float(box.get('xtl')))
+                    ymin = int(float(box.get('ytl')))
+                    xmax = int(float(box.get('xbr')))
+                    ymax = int(float(box.get('ybr')))
 
-                writer.addObject(label, xmin, ymin, xmax, ymax)
+                    writer.addObject(label, xmin, ymin, xmax, ymax)
 
-            anno_name = os.path.basename(os.path.splitext(image_name)[0] + '.xml')
-            anno_dir = os.path.dirname(os.path.join(output_dir, image_name))
-            os.makedirs(anno_dir, exist_ok=True)
-            writer.save(os.path.join(anno_dir, anno_name))
+                anno_name = os.path.basename(os.path.splitext(image_name)[0] + '.xml')
+                anno_dir = os.path.dirname(os.path.join(output_xml_dir, image_name))
+                os.makedirs(anno_dir, exist_ok=True)
+                writer.save(os.path.join(anno_dir, anno_name))
 
-    else:
-        for img_tag in cvat_xml.findall('image'):
-            image_name = img_tag.get('name')
-            width = img_tag.get('width')
-            height = img_tag.get('height')
-            image_path = os.path.join(image_dir, image_name)
-            if not os.path.exists(image_path):
-                log.warn('{} image cannot be found. Is `{}` image directory correct?'.
-                    format(image_path, image_dir))
-            writer = Writer(image_path, width, height)
+        else:
+            for img_tag in cvat_xml.findall('image'):
+                image_name = img_tag.get('name')
+                width = img_tag.get('width')
+                height = img_tag.get('height')
+                image_path = os.path.join(image_dir, image_name)
+                output_image_path = os.path.join(output_img_dir, image_name)
+                shutil.copy(image_path, output_image_path)
+                if not os.path.exists(image_path):
+                    log.warn('{} image cannot be found. Is `{}` image directory correct?'.
+                        format(image_path, image_dir))
+                writer = Writer(image_path, width, height)
 
-            unknown_tags = {x.tag for x in img_tag.iter()}.difference(KNOWN_TAGS)
-            if unknown_tags:
-                log.warn('Ignoring tags for image {}: {}'.format(image_path, unknown_tags))
+                unknown_tags = {x.tag for x in img_tag.iter()}.difference(KNOWN_TAGS)
+                if unknown_tags:
+                    log.warn('Ignoring tags for image {}: {}'.format(image_path, unknown_tags))
 
-            for box in img_tag.findall('box'):
-                label = box.get('label')
-                xmin = float(box.get('xtl'))
-                ymin = float(box.get('ytl'))
-                xmax = float(box.get('xbr'))
-                ymax = float(box.get('ybr'))
+                for box in img_tag.findall('box'):
+                    label = box.get('label')
+                    xmin = int(float(box.get('xtl')))
+                    ymin = int(float(box.get('ytl')))
+                    xmax = int(float(box.get('xbr')))
+                    ymax = int(float(box.get('ybr')))
 
-                writer.addObject(label, xmin, ymin, xmax, ymax)
+                    writer.addObject(label, xmin, ymin, xmax, ymax)
 
-            anno_name = os.path.basename(os.path.splitext(image_name)[0] + '.xml')
-            anno_dir = os.path.dirname(os.path.join(output_dir, image_name))
-            os.makedirs(anno_dir, exist_ok=True)
-            writer.save(os.path.join(anno_dir, anno_name))
+                anno_name = os.path.basename(os.path.splitext(image_name)[0] + '.xml')
+                anno_dir = os.path.dirname(os.path.join(output_xml_dir, image_name))
+                os.makedirs(anno_dir, exist_ok=True)
+                writer.save(os.path.join(anno_dir, anno_name))
 
 
 def main():
